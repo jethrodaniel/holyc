b715269baae57990df81b70a4605a92a7cee708f
Mark Delk <jethrodaniel@gmail.com>
2021-06-27 19:21:09 -0500

    rm uneeded main.s

--- main.s
+++
@@ -1,8 +0,0 @@
-; syscall table: https://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/
-
-bits 64
-
-_start:
-   mov rax, 60  ; exit
-   mov rdi, 42  ; arg1, exit code
-   syscall      ; exit()
