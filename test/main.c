26260388142566583cbba1d3530d79a59ada03db
Mark Delk <jethrodaniel@gmail.com>
2021-11-26 12:32:20 -0600

    rm a static var

--- test/main.c
+++ test/main.c
@@ -9,16 +9,15 @@ void test_fail(Test *t) {
   memcpy(t->err, msg, strlen(msg));
 }
 
-#define TEST(fn, desc) {fn, #fn " - " desc},
-
-static Test tests[] = {
-  TEST(test_pass, "this one passes")
-  TEST(test_fail, "this one fails")
-  TEST(test_pass, "this one passes")
-};
-
+#define TEST(fn) {fn, #fn},
 
 int main(int argc, char **argv) {
+  Test tests[] = {
+    TEST(test_pass)
+    TEST(test_fail)
+    TEST(test_pass)
+  };
+
   T *t = test_init(tests, sizeof(tests) / sizeof(Test), argc, argv);
   return run_tests(t);
 }
